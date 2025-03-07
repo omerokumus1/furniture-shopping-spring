9c16ba572d02c515756ee51da3021c5f4fbf62c5
Ömer Okumuş <omerokumus3@gmail.com>
2025-03-08 02:20:47 +0300

    ComponentScan added into FurnitureShoppingSpringApplication

--- src/main/java/com/example/starter/FurnitureShoppingSpringApplication.java
+++ src/main/java/com/example/starter/FurnitureShoppingSpringApplication.java
@@ -2,8 +2,10 @@ package com.example.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example")
 public class FurnitureShoppingSpringApplication {
 
 	public static void main(String[] args) {
