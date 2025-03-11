35799dae5d3c1231dfa46e287ddf1d97f81cfcd7
Ömer Okumuş <omerokumus3@gmail.com>
2025-03-12 00:59:06 +0300

    Annotations for FurnitureShoppingSpringApplication

--- src/main/java/com/omerokumus/starter/FurnitureShoppingSpringApplication.java
+++ src/main/java/com/omerokumus/starter/FurnitureShoppingSpringApplication.java
@@ -2,10 +2,14 @@ package com.omerokumus.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.omerokumus")
 @ComponentScan(basePackages = "com.omerokumus")
+@EnableJpaRepositories(basePackages = "com.omerokumus")
 public class FurnitureShoppingSpringApplication {
 
 	public static void main(String[] args) {
